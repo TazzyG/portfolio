e4ecbac5b10de67171aceb88f46908b101c2594b
laurie@friendlyroad.com <laurie@friendlyroad.com>
2017-06-08 01:36:51 -0600

    carousel with fade

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+	def full_title(page_title = '')
+  	base_title = "Haathi Consulting"
+  	meta = "Websites that grow with you"
+  	if page_title.empty?
+    	base_title + ":" + meta
+  	else
+    	page_title + " | " + base_title
+  	end
+  end
+  
+  def active_class(link_path)
+    current_page?(link_path) ? 'active' : ""
+  end
+
 end
