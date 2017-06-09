e8583c86dd7f8452a57881d07726b40985b6d77f
laurie@friendlyroad.com <laurie@friendlyroad.com>
2017-06-09 12:54:48 -0600

    added devise views, same as FR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params| 
+      user_params.permit(:password, :password_confirmation)
+    end
+    
+  end
 end
