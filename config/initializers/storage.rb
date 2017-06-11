af1047fd1ae0c8c5836f2cba9f4261ceff29905f
laurie@friendlyroad.com <laurie@friendlyroad.com>
2017-06-11 17:25:03 -0600

    typo in storage.rb

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'                    # required  
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["S3_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ca-central-1',
       # host: 's3.creativecalgary.ca'
       # endpoint: 'https://s3.creativecalgary.ca:8080'
