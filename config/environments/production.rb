b0a93c16f60871eacf20d77ddbd754c2d89f69ac
laurie@friendlyroad.com <laurie@friendlyroad.com>
2017-06-15 14:12:51 -0600

    removed change in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,6 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
-  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
