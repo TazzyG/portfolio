5425bd7894ae24294cf237bffe78a82633aa61ba
laurie@friendlyroad.com <laurie@friendlyroad.com>
2017-06-15 19:09:51 -0600

    add more lines for production.rb to asset load from s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,10 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   config.action_controller.asset_host = "//#{ENV['S3_BUCKET_NAME']}.s3.amazonaws.com"
+  config.assets.digest = true
+  config.assets.enabled = true
+  config.assets.initialize_on_precompile = true
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
