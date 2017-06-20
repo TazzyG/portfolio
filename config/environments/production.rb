d191d9ed19fc479d49ed18b478e2d14a4d9d8aa0
laurie@friendlyroad.com <laurie@friendlyroad.com>
2017-06-19 19:52:10 -0600

    copy prod.rb from git yesterday

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,12 +27,9 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
-  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=31536000" }
-
   
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
