3b73ce6bd1ee3973b8d1d67edd054b930a0c835c
laurie@friendlyroad.com <laurie@friendlyroad.com>
2017-06-19 19:20:11 -0600

    attempt 4 cloudfront prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host =  Proc.new { |source|
     if source =~ /\b(.png|.jpg|.gif)\b/i 
-      'd1aqxnjk8vqlqi.cloudfront.net'
+      'https://s.3.amazonaws.com/haath'
     end
 
   # Specifies the header that your server uses for sending files.
