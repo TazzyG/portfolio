e4b4bb5205f2ed068da42c6d3d5557ab0be4d2be
laurie@friendlyroad.com <laurie@friendlyroad.com>
2017-06-19 19:18:43 -0600

    attempt cloudfront prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,9 @@ Rails.application.configure do
   
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host =  Proc.new { |source|
-    if source =~ /\b(.png|.jpg|.gif)\b/i 'd1aqxnjk8vqlqi.cloudfront.net'
+    if source =~ /\b(.png|.jpg|.gif)\b/i 
+      'd1aqxnjk8vqlqi.cloudfront.net'
+    end
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
