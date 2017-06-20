036498eb53e7a930e377c024d181990f5cdeefaa
laurie@friendlyroad.com <laurie@friendlyroad.com>
2017-06-19 20:06:16 -0600

    cloudfront try after fixing prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  config.action_controller.asset_host =  'd1aqxnjk8vqlqi.cloudfront.net'
+
 
 
   # Specifies the header that your server uses for sending files.
