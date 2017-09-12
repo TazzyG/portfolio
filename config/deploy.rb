efdc51cb19c99fa271f7b01ff067d07e176c205a
Laurie Gloge <gloge@haathi-consulting.com>
2017-09-12 09:14:06 -0600

    change ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :pty,             true
 set :use_sudo,        false
 set :deploy_via, :remote_cache
 set :deploy_to, '/home/laurie/haathi-consulting'
-set :rvm_ruby_version, '2.4.0'
+set :rvm_ruby_version, '2.4.1'
 
 set :pty,             true
 set :use_sudo,        false
